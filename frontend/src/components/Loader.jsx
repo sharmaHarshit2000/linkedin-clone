03b38b2e330a918769e93e633372b148e3cb8de5
sharmaHarshit2000 <harshitsharma9989@gmail.com>
2025-08-03 23:53:00 +0530

    create Loader

---
+++ frontend/src/components/Loader.jsx
@@ -0,0 +1,16 @@
+import { Box, CircularProgress } from '@mui/material';
+
+const Loader = () => {
+  return (
+    <Box
+      display="flex"
+      justifyContent="center"
+      alignItems="center"
+      height="60vh"
+    >
+      <CircularProgress />
+    </Box>
+  );
+};
+
+export default Loader;
