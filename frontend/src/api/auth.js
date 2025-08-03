87e55cfe9eac29befb1bda6f708b8f9e537a8c68
sharmaHarshit2000 <harshitsharma9989@gmail.com>
2025-08-03 23:51:41 +0530

    move to getme route in auth.js file

--- frontend/src/api/auth.js
+++ frontend/src/api/auth.js
@@ -5,3 +5,6 @@ export const registerUser = (userData) => API.post('/auth/register', userData);
 
 // Login
 export const loginUser = (credentials) => API.post('/auth/login', credentials);
+
+// Get current user profile
+export const getMe = () => API.get('/auth/me');
