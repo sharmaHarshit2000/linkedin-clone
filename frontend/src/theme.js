6a975e4c52a9fc7998bfd98179b4f227f8b32172
sharmaHarshit2000 <harshitsharma9989@gmail.com>
2025-08-03 11:17:22 +0530

    setup the folder strcture in frontend

---
+++ frontend/src/theme.js
@@ -0,0 +1,21 @@
+import { createTheme } from "@mui/material/styles";
+
+const lightTheme = createTheme({
+  palette: {
+    mode: "light",
+    primary: {
+      main: "#1976d2",
+    },
+  },
+});
+
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#90caf9",
+    },
+  },
+});
+
+export { lightTheme, darkTheme };
