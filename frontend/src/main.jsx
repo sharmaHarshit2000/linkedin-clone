8ebe01c9766d51ddff9e182d684a7e0f4dd4bf13
sharmaHarshit2000 <harshitsharma9989@gmail.com>
2025-08-03 21:56:10 +0530

    add theme, toast and context

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,40 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { useState, useMemo } from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { ThemeProvider, CssBaseline } from '@mui/material';
+import { SnackbarProvider } from 'notistack';
+import { lightTheme, darkTheme } from './theme';
+import { AuthProvider } from './context/AuthContext';
+import { BrowserRouter } from 'react-router-dom'; 
+import { Toaster } from 'react-hot-toast';
+import "./index.css"
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const Root = () => {
+  const [mode, setMode] = useState('light');
+
+  const theme = useMemo(() => (mode === 'light' ? lightTheme : darkTheme), [mode]);
+
+  return (
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <SnackbarProvider>
+          <AuthProvider>
+            <App toggleTheme={() => setMode(mode === 'light' ? 'dark' : 'light')} mode={mode} />
+            <Toaster
+              position="top-right"
+              toastOptions={{
+                style: {
+                  padding: '12px 16px',
+                  fontSize: '14px',
+                },
+              }}
+            />
+          </AuthProvider>
+        </SnackbarProvider>
+      </ThemeProvider>
+    </BrowserRouter>
+  );
+};
+
+ReactDOM.createRoot(document.getElementById('root')).render(<Root />);
