1a15fd2c7215bbf50a1cf2b1a6536ecaa15dbbb4
sharmaHarshit2000 <harshitsharma9989@gmail.com>
2025-08-04 00:09:04 +0530

    implement lazy loading and route setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,63 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Box, Container, } from '@mui/material';
+import { lazy, Suspense } from 'react';
+import { Route, Routes } from 'react-router-dom';
 
-function App() {
-  const [count, setCount] = useState(0)
+import Header from './components/Header';
+import Footer from './components/Footer';
+import ProtectedRoute from './components/ProtectedRoute';
+import Loader from './components/Loader';
 
+// Lazy-loaded pages
+const Home = lazy(() => import('./pages/Home'));
+const Register = lazy(() => import('./pages/Register'));
+const Login = lazy(() => import('./pages/Login'));
+const CreatePost = lazy(() => import('./pages/CreatePost'));
+const UserProfile = lazy(() => import('./pages/UserProfile'));
+const MyProfile = lazy(() => import('./pages/MyProfile'));
+const NotFound = lazy(() => import('./pages/NotFound'));
+
+
+const App = ({ toggleTheme, mode }) => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <Box display="flex" flexDirection="column" minHeight="100vh">
+      <Header toggleTheme={toggleTheme} mode={mode} />
 
-export default App
+      <Box component="main" flexGrow={1} py={4}>
+        <Container maxWidth="md">
+          <Suspense fallback={<Loader />}>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
+
+              <Route
+                path="/create-post"
+                element={
+                  <ProtectedRoute>
+                    <CreatePost />
+                  </ProtectedRoute>
+                }
+              />
+
+              <Route
+                path="/profile"
+                element={
+                  <ProtectedRoute>
+                    <MyProfile />
+                  </ProtectedRoute>
+                }
+              />
+              <Route path="/profile/:id" element={<UserProfile />} />
+
+              <Route path="*" element={<NotFound />} />
+            </Routes>
+          </Suspense>
+        </Container>
+      </Box>
+
+      <Footer />
+    </Box>
+  );
+};
+
+export default App;
